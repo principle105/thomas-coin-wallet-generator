725378c3621e87c88bdfd1b06471eb1e356d25ee
principle105 <principle105@gmail.com>
2021-09-26 19:53:47 -0400

    fixed issue

--- static/script.js
+++ static/script.js
@@ -1,31 +1,20 @@
 let btn = document.getElementById("dark-theme-btn");
 let body = document.body;
 
-const textToAdd = "<span class='copied'>Copied!</span>"
-
 window.onload = function () {
-  if (localStorage.getItem("theme") === "dark") {
-    body.className += " dark-theme";
-    btn.innerHTML = "💡";
-  }
+    if (localStorage.getItem("theme") === "dark") {
+        body.className += " dark-theme";
+        btn.innerHTML = "💡";
+    }
 };
 
 btn.onclick = () => {
-  body.className = body.className ? "" : "dark-theme";
-  btn.innerHTML = body.className ? "💡" : "🌙";
-  localStorage.setItem("theme", body.className ? "dark" : "");
+    body.className = body.className ? "" : "dark-theme";
+    btn.innerHTML = body.className ? "💡" : "🌙";
+    localStorage.setItem("theme", body.className ? "dark" : "");
 };
 
 const copyToClipboard = (text) => {
-  const original = text.innerHTML;
-
-  const cb = navigator.clipboard;
-  cb.writeText(text.innerHTML);
-  
-  text.innerHTML += textToAdd;
-
-  setTimeout(function () {
-    text.innerHTML = original
-  }, 1000);
-  
+    const cb = navigator.clipboard;
+    cb.writeText(text.innerHTML);
 };
